88a74c7fa13d88875502b2c4f6207d5babd02cfa
ahmednazirmusah <ahmed999nazir@gmail.com>
2022-11-24 20:36:00 +0000

    fix erros

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import './index.css';
 import store from './redux/store';
 
 const container = document.getElementById('root');
